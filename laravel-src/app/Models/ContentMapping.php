7322151f4f782467589d219c0b879f0d9b4a0a0b
hartleyzuberi <hartleyzuberi@gmail.com>
2026-08-21 16:06:26 +0300

    Type content mapping provenance relation

--- laravel-src/app/Models/ContentMapping.php
+++ laravel-src/app/Models/ContentMapping.php
@@ -5,7 +5,19 @@ namespace App\Models;
 use App\Models\Concerns\HasUlidPrimaryKey;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Support\Carbon;
 
+/**
+ * @property string $id
+ * @property string $source_segment_id
+ * @property string $content_block_id
+ * @property string $mapping_type
+ * @property string|null $review_status
+ * @property string|null $reviewed_by
+ * @property Carbon|null $reviewed_at
+ * @property string|null $review_note
+ * @property-read SourceSegment $segment
+ */
 class ContentMapping extends Model
 {
     use HasUlidPrimaryKey;
@@ -17,6 +29,7 @@ class ContentMapping extends Model
         return ['reviewed_at' => 'datetime'];
     }
 
+    /** @return BelongsTo<SourceSegment, $this> */
     public function segment(): BelongsTo
     {
         return $this->belongsTo(SourceSegment::class, 'source_segment_id');
