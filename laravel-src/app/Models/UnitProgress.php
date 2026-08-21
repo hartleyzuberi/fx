126de8350d8f0ae819dbb89e59aa2ac5bd02bdb0
hartleyzuberi <hartleyzuberi@gmail.com>
2026-08-21 16:00:16 +0300

    Type unit-progress relation and attributes

--- laravel-src/app/Models/UnitProgress.php
+++ laravel-src/app/Models/UnitProgress.php
@@ -5,7 +5,19 @@ namespace App\Models;
 use App\Models\Concerns\HasUlidPrimaryKey;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Support\Carbon;
 
+/**
+ * @property string $id
+ * @property string $enrollment_id
+ * @property string $learning_unit_id
+ * @property string $status
+ * @property Carbon|null $started_at
+ * @property Carbon|null $passed_at
+ * @property Carbon|null $mastered_at
+ * @property Carbon|null $last_activity_at
+ * @property-read LearningUnit $unit
+ */
 class UnitProgress extends Model
 {
     use HasUlidPrimaryKey;
@@ -19,6 +31,7 @@ class UnitProgress extends Model
         return ['started_at' => 'datetime', 'passed_at' => 'datetime', 'mastered_at' => 'datetime', 'last_activity_at' => 'datetime'];
     }
 
+    /** @return BelongsTo<LearningUnit, $this> */
     public function unit(): BelongsTo
     {
         return $this->belongsTo(LearningUnit::class, 'learning_unit_id');
