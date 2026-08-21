93d67725395030fdd516d53b3872c65be76a372c
hartleyzuberi <hartleyzuberi@gmail.com>
2026-08-21 16:06:52 +0300

    Type source page provenance relation

--- laravel-src/app/Models/SourcePage.php
+++ laravel-src/app/Models/SourcePage.php
@@ -6,6 +6,14 @@ use App\Models\Concerns\HasUlidPrimaryKey;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
+/**
+ * @property string $id
+ * @property string $source_version_id
+ * @property int $physical_page
+ * @property string|null $logical_page
+ * @property array<string, mixed>|null $markers
+ * @property-read SourceVersion $version
+ */
 class SourcePage extends Model
 {
     use HasUlidPrimaryKey;
@@ -17,6 +25,7 @@ class SourcePage extends Model
         return ['markers' => 'array'];
     }
 
+    /** @return BelongsTo<SourceVersion, $this> */
     public function version(): BelongsTo
     {
         return $this->belongsTo(SourceVersion::class, 'source_version_id');
