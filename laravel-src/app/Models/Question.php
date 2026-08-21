ea1a0d6692b5579d27f90e44e85b66d92a333b0e
hartleyzuberi <hartleyzuberi@gmail.com>
2026-08-21 14:07:23 +0300

    Expose structured question metadata

--- laravel-src/app/Models/Question.php
+++ laravel-src/app/Models/Question.php
@@ -8,9 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 /**
  * @property string $id
  * @property string|null $concept_id
+ * @property string|null $learning_objective_id
+ * @property string $question_type
+ * @property string|null $structured_type
+ * @property string $structured_status
+ * @property string $question_version
  * @property string $prompt
  * @property float $points
+ * @property array<string, mixed>|null $choices
+ * @property array<string, mixed>|null $structured_choices
  * @property array<string, mixed>|null $answer_key
+ * @property array<string, mixed>|null $structured_answer_key
  */
 class Question extends Model
 {
@@ -20,6 +28,21 @@ class Question extends Model
 
     protected function casts(): array
     {
-        return ['choices' => 'array', 'answer_key' => 'array', 'requires_working' => 'boolean'];
+        return [
+            'choices' => 'array',
+            'structured_choices' => 'array',
+            'answer_key' => 'array',
+            'structured_answer_key' => 'array',
+            'metadata' => 'array',
+            'requires_working' => 'boolean',
+        ];
+    }
+
+    public function hasApprovedStructuredVariant(): bool
+    {
+        return $this->structured_status === 'approved'
+            && is_string($this->structured_type)
+            && $this->structured_type !== ''
+            && is_array($this->structured_answer_key);
     }
 }
