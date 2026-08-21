40a6b113bbd4b5e40b1d1159dd3f993f3b7c4235
hartleyzuberi <hartleyzuberi@gmail.com>
2026-08-21 16:00:03 +0300

    Type course relations and attributes

--- laravel-src/app/Models/Course.php
+++ laravel-src/app/Models/Course.php
@@ -3,9 +3,18 @@
 namespace App\Models;
 
 use App\Models\Concerns\HasUlidPrimaryKey;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
+/**
+ * @property string $id
+ * @property string $slug
+ * @property string $title
+ * @property string|null $description
+ * @property bool $is_active
+ * @property-read Collection<int, CurriculumVersion> $versions
+ */
 class Course extends Model
 {
     use HasUlidPrimaryKey;
@@ -17,6 +26,7 @@ class Course extends Model
         return ['is_active' => 'boolean'];
     }
 
+    /** @return HasMany<CurriculumVersion, $this> */
     public function versions(): HasMany
     {
         return $this->hasMany(CurriculumVersion::class);
