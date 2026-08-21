658190db98edfca115d954c015a2ade7aca58ad6
hartleyzuberi <hartleyzuberi@gmail.com>
2026-08-21 15:59:16 +0300

    Type content-block relations and attributes

--- laravel-src/app/Models/ContentBlock.php
+++ laravel-src/app/Models/ContentBlock.php
@@ -3,10 +3,23 @@
 namespace App\Models;
 
 use App\Models\Concerns\HasUlidPrimaryKey;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
+/**
+ * @property string $id
+ * @property string $learning_unit_id
+ * @property string $block_type
+ * @property int $position
+ * @property string|null $title
+ * @property string|null $body
+ * @property array<string, mixed>|null $payload
+ * @property bool $is_required
+ * @property-read LearningUnit $unit
+ * @property-read Collection<int, ContentMapping> $mappings
+ */
 class ContentBlock extends Model
 {
     use HasUlidPrimaryKey;
@@ -18,11 +31,13 @@ class ContentBlock extends Model
         return ['payload' => 'array', 'is_required' => 'boolean'];
     }
 
+    /** @return BelongsTo<LearningUnit, $this> */
     public function unit(): BelongsTo
     {
         return $this->belongsTo(LearningUnit::class, 'learning_unit_id');
     }
 
+    /** @return HasMany<ContentMapping, $this> */
     public function mappings(): HasMany
     {
         return $this->hasMany(ContentMapping::class);
