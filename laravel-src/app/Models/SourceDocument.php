77142b4d256134cfc31aae13be6ca655aec1151c
hartleyzuberi <hartleyzuberi@gmail.com>
2026-08-21 16:07:15 +0300

    Type source document attributes

--- laravel-src/app/Models/SourceDocument.php
+++ laravel-src/app/Models/SourceDocument.php
@@ -5,6 +5,11 @@ namespace App\Models;
 use App\Models\Concerns\HasUlidPrimaryKey;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @property string $id
+ * @property string $title
+ * @property bool $is_canonical
+ */
 class SourceDocument extends Model
 {
     use HasUlidPrimaryKey;
