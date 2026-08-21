dbfce6f8d2b28c109bf9fb701d02accb410bdcbd
hartleyzuberi <hartleyzuberi@gmail.com>
2026-08-21 16:06:39 +0300

    Type source segment provenance relation

--- laravel-src/app/Models/SourceSegment.php
+++ laravel-src/app/Models/SourceSegment.php
@@ -6,6 +6,14 @@ use App\Models\Concerns\HasUlidPrimaryKey;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
+/**
+ * @property string $id
+ * @property string $source_page_id
+ * @property array<string, mixed>|null $heading_hierarchy
+ * @property array<string, mixed>|null $metadata
+ * @property bool $is_meaningful
+ * @property-read SourcePage $page
+ */
 class SourceSegment extends Model
 {
     use HasUlidPrimaryKey;
@@ -17,6 +25,7 @@ class SourceSegment extends Model
         return ['heading_hierarchy' => 'array', 'metadata' => 'array', 'is_meaningful' => 'boolean'];
     }
 
+    /** @return BelongsTo<SourcePage, $this> */
     public function page(): BelongsTo
     {
         return $this->belongsTo(SourcePage::class, 'source_page_id');
