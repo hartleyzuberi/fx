2b33149bb601a40b46fdc0bc6d48a37a42c591fa
hartleyzuberi <hartleyzuberi@gmail.com>
2026-08-21 16:07:04 +0300

    Type source version provenance relation

--- laravel-src/app/Models/SourceVersion.php
+++ laravel-src/app/Models/SourceVersion.php
@@ -5,7 +5,15 @@ namespace App\Models;
 use App\Models\Concerns\HasUlidPrimaryKey;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Support\Carbon;
 
+/**
+ * @property string $id
+ * @property string $source_document_id
+ * @property array<string, mixed>|null $metadata
+ * @property Carbon|null $imported_at
+ * @property-read SourceDocument $document
+ */
 class SourceVersion extends Model
 {
     use HasUlidPrimaryKey;
@@ -17,6 +25,7 @@ class SourceVersion extends Model
         return ['metadata' => 'array', 'imported_at' => 'datetime'];
     }
 
+    /** @return BelongsTo<SourceDocument, $this> */
     public function document(): BelongsTo
     {
         return $this->belongsTo(SourceDocument::class, 'source_document_id');
