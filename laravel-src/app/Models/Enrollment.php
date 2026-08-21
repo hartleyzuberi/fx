4f0f477ce8543df93ed981e18bf41bf3db3a3613
hartleyzuberi <hartleyzuberi@gmail.com>
2026-08-21 15:59:31 +0300

    Type enrollment relations and attributes

--- laravel-src/app/Models/Enrollment.php
+++ laravel-src/app/Models/Enrollment.php
@@ -3,10 +3,22 @@
 namespace App\Models;
 
 use App\Models\Concerns\HasUlidPrimaryKey;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Support\Carbon;
 
+/**
+ * @property string $id
+ * @property string $user_id
+ * @property string $curriculum_version_id
+ * @property string $status
+ * @property Carbon|null $started_at
+ * @property Carbon|null $completed_at
+ * @property-read CurriculumVersion $version
+ * @property-read Collection<int, UnitProgress> $progress
+ */
 class Enrollment extends Model
 {
     use HasUlidPrimaryKey;
@@ -18,11 +30,13 @@ class Enrollment extends Model
         return ['started_at' => 'datetime', 'completed_at' => 'datetime'];
     }
 
+    /** @return BelongsTo<CurriculumVersion, $this> */
     public function version(): BelongsTo
     {
         return $this->belongsTo(CurriculumVersion::class, 'curriculum_version_id');
     }
 
+    /** @return HasMany<UnitProgress, $this> */
     public function progress(): HasMany
     {
         return $this->hasMany(UnitProgress::class);
